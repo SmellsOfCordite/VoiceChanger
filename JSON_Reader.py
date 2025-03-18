ea5216fea09d7211bd8b92d5b606fb29fc2c46e0
SmellsOfCordite <philip.jorden@gmail.com>
2025-03-18 20:32:07 +0000

    Reads and writes to JSON file, easy peasy

--- JSON_Reader.py
+++ JSON_Reader.py
@@ -1,13 +1,21 @@
 import json
  
-# Opening JSON file
-with open('Effect_Sets.json', 'r') as openfile:
+def read_JSON(filepath):
+    print("Reading from JSON ", str(filepath))
+    with open(filepath, 'r') as openfile:
+        # Reading from json file
+        data = json.load(openfile)
+        return data
  
-    # Reading from json file
-    effects = json.load(openfile)
- 
-print(effects)
-print(type(effects))
 
-with open("Effect_Sets.json", mode="w", encoding="utf-8") as write_file:
-    json.dump(effects, write_file, indent = 2)
+def write_JSON(filepath, data):
+    print("Writing (Prettily) to JSON ", str(filepath))
+    with open(filepath, mode="w", encoding="utf-8") as write_file:
+        json.dump(data, write_file, indent = 2)
+
+filepath = "Effect_Sets.json"
+data = ""
+data = read_JSON(filepath)
+write_JSON(filepath,data)
+
+print(data)
